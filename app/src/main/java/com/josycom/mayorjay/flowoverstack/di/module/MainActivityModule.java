c4b943f06bf1c884f1fff4ed800e7ae1878719fd
Eunice B <eunicebakare5@gmail.com>
2020-10-27 14:23:48 +0100

    Added dagger for answer activity

--- app/src/main/java/com/josycom/mayorjay/flowoverstack/di/module/MainActivityModule.java
+++ app/src/main/java/com/josycom/mayorjay/flowoverstack/di/module/MainActivityModule.java
@@ -1,16 +1,17 @@
 package com.josycom.mayorjay.flowoverstack.di.module;
 
+import com.josycom.mayorjay.flowoverstack.ui.AnswerActivity;
 import com.josycom.mayorjay.flowoverstack.ui.MainActivity;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
 @Module
-public abstract class ActivityModule {
+public abstract class MainActivityModule {
 
 
     @ContributesAndroidInjector(modules = QuestionsFragmentModule.class)
-    abstract MainActivity contributeYourAndroidInjector();
+    abstract MainActivity contributeMainAndroidInjector();
 
 }
 
