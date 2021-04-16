817b35fe2a7bb23caa005ec736c5d72c57161a3f
mayorjay <joseolu4gsm@yahoo.com>
2021-04-16 09:55:39 +0100

    feat: Support in-app updates

--- app/src/main/java/com/josycom/mayorjay/flowoverstack/ui/activity/MainActivity.java
+++ app/src/main/java/com/josycom/mayorjay/flowoverstack/ui/activity/MainActivity.java
@@ -1,8 +1,17 @@
 package com.josycom.mayorjay.flowoverstack.ui.activity;
 
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.InstallState;
+import com.google.android.play.core.install.InstallStateUpdatedListener;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.InstallStatus;
+import com.google.android.play.core.install.model.UpdateAvailability;
 import com.josycom.mayorjay.flowoverstack.R;
 import com.josycom.mayorjay.flowoverstack.databinding.ActivityMainBinding;
 import com.josycom.mayorjay.flowoverstack.ui.fragment.QuestionsByActivityFragment;
@@ -12,10 +21,12 @@ import com.josycom.mayorjay.flowoverstack.ui.fragment.QuestionsByVoteFragment;
 import com.josycom.mayorjay.flowoverstack.util.AppConstants;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -40,6 +51,8 @@ public class MainActivity extends AppCompatActivity implements HasAndroidInjecto
     private boolean isFabOpen = false;
     private ActivityMainBinding mActivityMainBinding;
     private Animation fabOpen, fabClose;
+    private AppUpdateManager mAppUpdateManager;
+    private static final int APP_UPDATE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +88,12 @@ public class MainActivity extends AppCompatActivity implements HasAndroidInjecto
                 isFragmentDisplayed = true;
             }
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkForUpdate();
     }
 
     private void fabAction() {
@@ -165,6 +184,74 @@ public class MainActivity extends AppCompatActivity implements HasAndroidInjecto
         if (isTablet) {
             mActivityMainBinding.scanToSearch.setTextSize(15F);
             mActivityMainBinding.typeToSearch.setTextSize(15F);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAppUpdateManager != null) {
+            mAppUpdateManager.unregisterListener(installStateUpdatedListener);
+        }
+    }
+
+    private InstallStateUpdatedListener installStateUpdatedListener = new InstallStateUpdatedListener() {
+        @Override
+        public void onStateUpdate(@NonNull InstallState state) {
+            if (state.installStatus() == InstallStatus.DOWNLOADED) {
+                popupSnackBarForCompleteUpdate();
+            } else if (state.installStatus() == InstallStatus.INSTALLED) {
+                if (mAppUpdateManager != null) {
+                    mAppUpdateManager.unregisterListener(installStateUpdatedListener);
+                }
+            } else {
+                Log.i("UpdateInstaller", "InstallStateUpdatedListener >>>>> " + state.installStatus());
+            }
+        }
+    };
+
+    private void checkForUpdate() {
+        mAppUpdateManager = AppUpdateManagerFactory.create(this);
+        mAppUpdateManager.registerListener(installStateUpdatedListener);
+        mAppUpdateManager.getAppUpdateInfo().addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                    && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                try {
+                    mAppUpdateManager.startUpdateFlowForResult(
+                            appUpdateInfo, AppUpdateType.FLEXIBLE, MainActivity.this, APP_UPDATE);
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
+                }
+            } else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                popupSnackBarForCompleteUpdate();
+            } else {
+                Log.e("UpdateInstaller", "checkForAppUpdateAvailability >>>>>" + appUpdateInfo.installStatus());
+            }
+        });
+    }
+
+    private void popupSnackBarForCompleteUpdate() {
+        Snackbar snackbar = Snackbar.make(
+                        findViewById(R.id.main_root_layout),
+                        "An update has just been downloaded",
+                        Snackbar.LENGTH_INDEFINITE);
+
+        snackbar.setAction("Restart", view -> {
+            if (mAppUpdateManager != null) {
+                mAppUpdateManager.completeUpdate();
+            }
+        });
+        snackbar.setActionTextColor(getResources().getColor(R.color.colorPrimaryText));
+        snackbar.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_UPDATE) {
+            if (resultCode != RESULT_OK) {
+                checkForUpdate();
+            }
         }
     }
 }
