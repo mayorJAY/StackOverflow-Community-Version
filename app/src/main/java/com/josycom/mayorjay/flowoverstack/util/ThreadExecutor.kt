459e70ba5e4b4c4ea88934ec31516d5632f8f7d4
mayorjay <joseolu4gsm@yahoo.com>
2021-10-26 13:50:16 +0100

    feat: Migrate codebase from Java to Kotlin

--- app/src/main/java/com/josycom/mayorjay/flowoverstack/util/ThreadExecutor.kt
+++ app/src/main/java/com/josycom/mayorjay/flowoverstack/util/ThreadExecutor.kt
@@ -4,6 +4,5 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 object ThreadExecutor {
-    @JvmField
     var mExecutor: ExecutorService = Executors.newFixedThreadPool(5)
 }
