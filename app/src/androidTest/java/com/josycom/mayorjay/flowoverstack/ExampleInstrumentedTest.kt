91cc434984986d9b97d56be5157bdf0726d5735e
mayorjay <joseolu4gsm@yahoo.com>
2022-02-12 15:51:00 +0100

    feat: Write Unit and UI tests

--- app/src/androidTest/java/com/josycom/mayorjay/flowoverstack/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/josycom/mayorjay/flowoverstack/ExampleInstrumentedTest.kt
@@ -1,27 +1,22 @@
-package com.josycom.mayorjay.flowoverstack;
+package com.josycom.mayorjay.flowoverstack
 
-import static org.junit.Assert.assertEquals;
-
-import android.content.Context;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-
-        assertEquals("com.example.josycom.flowoverstack", appContext.getPackageName());
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.josycom.mayorjay.flowoverstack", appContext.packageName)
     }
 }
--- app/src/androidTest/java/com/josycom/mayorjay/flowoverstack/ScreenNavigationTest.kt
+++ app/src/androidTest/java/com/josycom/mayorjay/flowoverstack/ScreenNavigationTest.kt
@@ -1,41 +1,76 @@
-package com.josycom.mayorjay.flowoverstack;
+package com.josycom.mayorjay.flowoverstack
 
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.josycom.mayorjay.flowoverstack.ui.activity.MainActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import com.josycom.mayorjay.flowoverstack.ui.activity.MainActivity;
+@RunWith(AndroidJUnit4::class)
+class ScreenNavigationTest {
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+    @get:Rule
+    val scenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    private var scenario: ActivityScenario<MainActivity>? = null
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+    @Before
+    fun setup() {
+        scenario = scenarioRule.scenario
+    }
 
-@RunWith(AndroidJUnit4.class)
-public class ActivityLaunchTest {
-
-    @Rule
-    public ActivityTestRule mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-    @Test
-    public void launchQuestionByCreationFragment(){
-        openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
-        onView(withText(R.string.action_filter_by_recency)).perform(click());
-        onView(withId(R.id.tv_recent_questions)).check(matches(isDisplayed()));
+    @After
+    fun tearDown() {
+        scenario?.close()
     }
 
     @Test
-    public void launchSearchActivity(){
-        onView(withId(R.id.search_fab)).perform(click());
-        onView(withId(R.id.type_to_search)).perform(click());
-        onView(withId(R.id.search_text_input_layout)).check(matches(isDisplayed()));
+    fun test_clickMenuOption_displayQuestionsFilteredByCreation() {
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+        Espresso.onView(ViewMatchers.withText(R.string.action_filter_by_recency)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(R.string.recent_questions)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_clickMenuOption_displayQuestionsFilteredByHot() {
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+        Espresso.onView(ViewMatchers.withText(R.string.action_filter_by_hot)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(R.string.hot_questions)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_clickMenuOption_displayQuestionsFilteredByVotes() {
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+        Espresso.onView(ViewMatchers.withText(R.string.action_filter_by_vote)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(R.string.voted_questions)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_clickMenuOption_displayPopularTagsFragment() {
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+        Espresso.onView(ViewMatchers.withText(R.string.filter_by_popular_tags)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withText(R.string.select_a_tag_to_view_the_related_questions)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_clickMenuOption_displaySearchTagsFragment() {
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+        Espresso.onView(ViewMatchers.withText(R.string.filter_by_tag_name)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.search_text_input_layout)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun test_clickSearchFab_launchSearchActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.search_fab)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.type_to_search)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.search_text_input_layout)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
